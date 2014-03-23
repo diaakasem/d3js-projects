0321f778f4650d651e7030d937949d69214072fa
Diaa Kasem <me@diaa.me>
2014-03-23 20:44:34 +0200

    Fixing chart with the new requirements

--- bubblechart/app/js/bubblechart.js
+++ bubblechart/app/js/bubblechart.js
@@ -22,14 +22,14 @@
         var startX = x - w;
         var startY = y - h;
         var data = [
-        {x: startX + (w/2), y: startY},
-        {x: startX + width - (w/2), y: startY},
-        {x: startX + width - (w/2), y: startY + height - (height / 3)},
-        {x: startX + width, y: startY + height - (height / 3)},
-        {x: x, y: startY + height},
-        {x: startX, y: startY + height - (height / 3)},
-        {x: startX + (w/2), y: startY + height - (height / 3)},
-        {x: startX + (w/2), y: startY}
+            {x: startX + (w/2), y: startY},
+            {x: startX + width - (w/2), y: startY},
+            {x: startX + width - (w/2), y: startY + height - (height / 3)},
+            {x: startX + width, y: startY + height - (height / 3)},
+            {x: x, y: startY + height},
+            {x: startX, y: startY + height - (height / 3)},
+            {x: startX + (w/2), y: startY + height - (height / 3)},
+            {x: startX + (w/2), y: startY}
         ];
 
         var lineFunction = d3.svg.line()
@@ -89,10 +89,10 @@
         });
 
         var margin = _.defaults(configs.margins || {}, {
-            top: 20,
-            right: 20,
+            top: 50,
+            right: 40,
             bottom: 20,
-            left: 20
+            left: 40
         });
 
         // Declaring data and adding defaults
@@ -107,6 +107,12 @@
             throw "Please, set the selector where the graph should show up.";
         }
 
+        // Creating scales and Axises
+        x = d3.scale.linear().range([margin.left, width - margin.right]);
+        y = d3.scale.linear().range([height - margin.bottom - 20, margin.top + 20]);
+        r = d3.scale.linear().range([0, radius]);
+        r.domain([0, _.max(data.volume)]);
+
         // Defining limits
         var xStart = _.min(data.times);
         var xEnd = _.max(data.times);
@@ -120,7 +126,13 @@
             if (strike < minPrice || strike > maxPrice) {
                 return;
             }
-            strikeMaxPrice = Math.max(strikeMaxPrice, _.max(option.price) + strike);
+            if (strikesMode) {
+                var maxR = 2 * r(_.max(option.volume) / optionVolumeScale);
+                var maxStrike = y.invert(y(strike) - maxR);
+                strikeMaxPrice = Math.max(strikeMaxPrice, maxStrike);
+            } else {
+                strikeMaxPrice = Math.max(strikeMaxPrice, strike + _.max(option.price));
+            }
         });
 
         _.each(data.putPricesByStrike, function(option, key) {
@@ -128,13 +140,14 @@
             if (strike < minPrice || strike > maxPrice) {
                 return;
             }
-            strikeMinPrice = Math.min(strikeMinPrice, strike - _.min(option.price));
+            if (strikesMode) {
+                var maxR = 2 * r(_.max(option.volume) / optionVolumeScale);
+                var minStrike = y.invert(y(strike) + maxR);
+                strikeMinPrice = Math.min(strikeMinPrice, minStrike);
+            } else {
+                strikeMinPrice = Math.min(strikeMinPrice, strike - _.min(option.price));
+            }
         });
-
-        // Creating scales and Axises
-        x = d3.scale.linear().range([margin.left, width - margin.right]);
-        y = d3.scale.linear().range([height - margin.bottom, margin.top]);
-        r = d3.scale.linear().range([0, radius]);
 
         var xAxis = d3.svg.axis()
             .scale(x)
@@ -143,26 +156,34 @@
                 return moment(d).format("HH:mm");
             });
 
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left")
-            .outerTickSize(0);
-
         x.domain([xStart, xEnd]);
-        y.domain([Math.floor(strikeMinPrice), Math.ceil(strikeMaxPrice)]);
-        r.domain([0, _.max(data.volume)]);
+        if (strikesMode) {
+            y.domain([strikeMinPrice, strikeMaxPrice]);
+        } else {
+            y.domain([Math.floor(strikeMinPrice), Math.ceil(strikeMaxPrice)]);
+        }
 
         var svg = d3.select(configs.selector).append("svg")
-            .attr("viewBox", "0 0 " + configs.width + " " + configs.height)
+            //.attr("viewBox", -(margin.left) + " " + (-1 * margin.top ) + " " + (configs.width + margin.right) + " " + (configs.height + margin.bottom))
+            .attr("viewBox", "0 0 " + configs.width + " " + (configs.height))
             .attr("xmlns","http://www.w3.org/2000/svg");
 
-        var mainSvg = svg.append("g")
-            .attr("class", "mainSvg")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var headerSvg = svg.append("g").attr("class", "header-svg")
+            .attr("viewBox", margin.left + " 0 " +
+                    (configs.width - margin.left) + " " +
+                    margin.top);
 
-        var textSvg = svg.append("g")
-            .attr("class", "text-svg")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var mainSvg = svg.append("g")
+            .attr("class", "main-svg")
+            .attr("viewBox", margin.left + " " + margin.top + " " +
+                    (configs.width - margin.left - margin.right) + " " +
+                    (configs.height - margin.top - margin.bottom));
+
+        var footerSvg = svg.append("g").attr("class", "text-svg")
+            .attr("viewBox", margin.left + " " +
+                    (configs.height - margin.bottom) + " " +
+                    (configs.width - margin.left - margin.right) + " " +
+                    margin.bottom);
 
         mainPoints = _.map(data.prices, function(price, i) {
             return {
@@ -177,7 +198,8 @@
             .attr("transform", "translate(0, " + y(data.meanPrice) + ")")
             .call(xAxis)
             .select("path")
-            .attr("marker-end", function(d) { return "url(#axisRightArrow)"; });
+            .attr("marker-start", function(d) { return "url(#axisLeftArrow)"; })
+            .attr("marker-end", function(d) { return "url(#axisLeftArrow)"; });
 
         pointsFor(x, y, r, mainPoints, mainSvg, 'underlying-price-point');
 
@@ -196,7 +218,7 @@
             var callPoints = _.compact(_.map(value.price, function(price, i) {
                 var volume = value.volume[i] / optionVolumeScale;
                 price = strike + price;
-                if (price < strikeMinPrice || price > strikeMaxPrice) {
+                if (!strikesMode && (price < strikeMinPrice || price > strikeMaxPrice)) {
                     return null;
                 }
                 return {
@@ -221,9 +243,9 @@
             var css = 'call-' + strike + '-volume-point';
             pointsFor(x, callY, r, callPoints, mainSvg, css, strike);
 
-            textSvg.append("text")
+            mainSvg.append("text")
                 .attr("class", "strike-text")
-                .attr("transform", "translate(" + (x(xStart) - 20) + ", " + (y(strike) + 5) + ")")
+                .attr("transform", "translate(" + (x(xStart) - margin.left) + ", " + (y(strike) + 5) + ")")
                 .style("text-anchor", "start")
                 .text(strike);
         });
@@ -243,7 +265,7 @@
             var putPoints = _.compact(_.map(value.price, function(price, i) {
                 var volume = value.volume[i] / optionVolumeScale;
                 price = strike - price;
-                if (price < strikeMinPrice || price > strikeMaxPrice) {
+                if (!strikesMode && (price < strikeMinPrice || price > strikeMaxPrice)) {
                     return null;
                 }
                 return {
@@ -262,111 +284,114 @@
             pointsFor(x, putY, r, putPoints, mainSvg, css, strike);
         });
 
-        textSvg.append("text")
+        // Mean Text
+        mainSvg.append("text")
+            .attr("class", "mean-value-text")
+            .attr("transform", "translate(" + (x(xEnd) + margin.right) + ", " + (y(data.meanPrice)) + ")")
+            .style("text-anchor", "start")
+            .text((data.meanPrice || 0).toFixed(2));
+
+        mainSvg.append("text")
+            .attr("class", "mean-text")
+            .attr("transform", "translate(" + (x(xEnd) + margin.right) + ", " + (y(data.meanPrice) + 15) + ")")
+            .style("text-anchor", "start")
+            .text("VWAP");
+
+        // Company Underlying Text
+        headerSvg.append("text")
             .attr("class", "underlying-text")
-            .attr("transform", "translate(" + x(xStart) + ", 0)")
+            .attr("transform", "translate(" + (x(xStart) - margin.left) + ", " + (margin.top - 15) + ")")
             .style("text-anchor", "start")
             .text(data.underlying);
 
-        textSvg.append("text")
+        headerSvg.append("text")
             .attr("class", "underlying-company-name-text")
-            .attr("transform", "translate(" + x(xStart) + ", 15)")
+            .attr("transform", "translate(" + (x(xStart) - margin.left) + ", " + margin.top + ")")
             .style("text-anchor", "start")
             .text(data.underlyingCompanyName);
 
-        textSvg.append("text")
+        // Time
+        headerSvg.append("text")
             .attr("class", "time-text")
-            .attr("transform", "translate(" + x(xEnd) + ", 0)")
+            .attr("transform", "translate(" + (x(xEnd) + margin.left + margin.right) + ", " + 10 + ")")
             .style("text-anchor", "end")
             .text(moment(data.time).format('HH:mm:ss'));
 
-        textSvg.append("text")
+        headerSvg.append("text")
             .attr("class", "daily-price-text")
-            .attr("transform", "translate(" + (x((xStart + xEnd) / 2.0) - 10) + ", 0)")
+            .attr("transform", "translate(" + (x((xStart + xEnd) / 2.0) - 10) + ", " + 20 + ")")
             .style("text-anchor", "end")
             .text(data.price);
 
-        textSvg.append("text")
+        headerSvg.append("text")
             .attr("class", "daily-price-change-text")
-            .attr("transform", "translate(" + (x((xStart + xEnd) / 2.0) + 10) + ", 0)")
+            .attr("transform", "translate(" + (x((xStart + xEnd) / 2.0) + 10) + ", " + 20 + ")")
             .style("text-anchor", "start")
             .text(data.priceChange);
 
         if (data.priceChange < 0 ) {
-            downArrow(textSvg, x((xStart + xEnd) / 2.0), 0, 8, 20);
+            downArrow(headerSvg, x((xStart + xEnd) / 2.0), 15, 8, 20);
         } else { 
-            upArrow(textSvg, x((xStart + xEnd) / 2.0), 0, 8, 20);
+            upArrow(headerSvg, x((xStart + xEnd) / 2.0), 15, 8, 20);
         }
 
          // Lower table
-        var body = textSvg.append("foreignObject")
+        var body = footerSvg.append("foreignObject")
             .attr("class", "footer-text")
-            .attr("x", x(xStart))
-            .attr("y", height - 200)
-            .attr("width", width - margin.left - margin.right)
-            .attr("height", 200);
+            .attr("x", 0)
+            .attr("y", configs.height - margin.bottom - 50)
+            .attr("width", configs.width - margin.left)
+            .attr("height", 50);
 
-        body.append("xhtml:div")
-            .text("&Delta;t = 60 minutes");
-        body.append("xhtml:div")
-            .text("IV = 60 minutes");
-        body.append("xhtml:div")
-            .text("&Delta;t = 60 minutes");
-        body.append("xhtml:div")
-            .text("&Delta;t = 60 minutes");
+        function toFixed(float) {
+            return (float * 100).toFixed(1);
+        }
+        var firstRow = body.append("xhtml:div")
+            .attr("class", "first-row");
 
-        
+        var secondRow = body.append("xhtml:div")
+            .attr("class", "second-row");
+
+        firstRow.append("xhtml:span")
+            .attr("class", "first-span")
+            .html("&Delta;t = " + (data.timeStep / 1000 / 60) + " minutes");
+
+        var secondSpan = firstRow.append("xhtml:span")
+            .attr("class", "second-span");
+        secondSpan.append("xhtml:span")
+            .style("text-decoration", "overline")
+            .html("IV");
+        secondSpan.append("xhtml:sub")
+            .html("c");
+        secondSpan.append("xhtml:span")
+            .text(" = " + toFixed(data.callVolatilityModel.meanIV) + " % ");
+
+        secondRow.append("xhtml:span")
+            .attr("class", "first-span")
+            .html("&sigma; = " + toFixed(data.sigma) + " % ");
+
+        var forthSpan = secondRow.append("xhtml:span")
+            .attr("class", "forth-span");
+        forthSpan.append("xhtml:span")
+            .style("text-decoration", "overline")
+            .html("IV");
+        forthSpan.append("xhtml:sub")
+            .html("p");
+        forthSpan.append("xhtml:span")
+            .text(" = " + toFixed(data.putVolatilityModel.meanIV) + " % ");
+
         var defs = svg.append("defs");
 
         defs.append("marker")
-            .attr("id", "forecastArrow")
+            .attr("id", "axisLeftArrow")
             .attr("viewBox", "0 -5 10 10")
-            .attr("refX", 8)
-            .attr("refY", 0)
-            .attr("markerWidth", 4)
-            .attr("markerHeight", 3)
-            .attr("orient", "auto")
+            .attr("refX", 3)
+            .attr("refY", -4)
+            .attr("markerWidth", 20)
+            .attr("markerHeight", 15)
+            .attr("orient", 180)
             .append("path")
             .attr("class", 'forecastArrow')
             .attr("d", "M0,-5L10,0L0,5L2.5,0");
-
-        // Need a separate marker in order to color differently
-        // http://stackoverflow.com/a/16665510
-        defs.append("marker")
-            .attr("id", "axisRightArrow")
-            .attr("viewBox", "0 -5 10 10")
-            .attr("refX", 4)
-            .attr("refY", 0)
-            .attr("markerWidth", 12)
-            .attr("markerHeight", 9)
-            .attr("orient", "0")
-            .append("path")
-            .attr("class", 'axisArrow')
-            .attr("d", "M0,-5L10,0L0,5L2.5,0");
-
-        defs.append("marker")
-            .attr("id", "axisUpArrow")
-            .attr("viewBox", "0 -5 10 10")
-            .attr("refX", 4)
-            .attr("refY", 0)
-            .attr("markerWidth", 12)
-            .attr("markerHeight", 9)
-            .attr("orient", "270")
-            .append("path")
-            .attr("class", 'axisArrow')
-            .attr("d", "M0,-5L10,0L0,5L2.5,0");
-
-        defs.append("marker")
-            .attr("id", "axisDownArrow")
-            .attr("viewBox", "0 -5 10 10")
-            .attr("refX", 4)
-            .attr("refY", 0)
-            .attr("markerWidth", 12)
-            .attr("markerHeight", 9)
-            .attr("orient", "90")
-            .append("path")
-            .attr("class", 'axisArrow')
-            .attr("d", "M0,-5L10,0L0,5L2.5,0");
-
     };
 }(window));
