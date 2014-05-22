4774e80ed2ed089d0112ff6c7f6a8bec1e6f4514
Diaa Kasem <me@diaa.me>
2014-05-22 16:05:25 +0200

    Adding area chart

---
+++ area/main.js
@@ -0,0 +1,286 @@
+
+$(document).ready(function () {
+
+    var format = d3.time.format("%m/%d/%y");
+    var margin = {top: 40, right: 35, bottom: 20, left: 45},
+        width = 960 - margin.left - margin.right,
+        height = 250 - margin.top - margin.bottom;
+
+    var formatDollar = d3.format("$0,000");
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var z = d3.scale.category20c();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(d3.time.days).ticks(5);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(formatDollar);
+
+    var stack = d3.layout.stack()
+        .offset("zero")
+        .values(function (d) {
+            return d.values;
+        })
+        .x(function (d) {
+            return d.date;
+        })
+        .y(function (d) {
+            return d.value;
+        });
+
+    var nest = d3.nest()
+        .key(function (d) {
+            return d.key;
+        });
+
+    var area = d3.svg.area()
+        .interpolate("monotone")
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y0(function (d) {
+            return y(d.y0);
+        })
+        .y1(function (d) {
+            return y(d.y0 + d.y);
+        });
+
+    var svg = d3.select(".vis").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var data = [
+        {"key": "Setup", "value": 496, "date": "04\/01\/14", "clients": [
+            ["client7", 46],
+            ["client9", 52],
+            ["client1", 49],
+            ["client1", 86]
+        ]},
+        {"key": "Monthly", "value": 386, "date": "04\/01\/14", "clients": [
+            ["client3", 14],
+            ["client2", 88],
+            ["client2", 74],
+            ["client4", 32]
+        ]},
+        {"key": "Setup", "value": 199, "date": "04\/02\/14", "clients": [
+            ["client8", 91]
+        ]},
+        {"key": "Monthly", "value": 99, "date": "04\/02\/14", "clients": [
+            ["client2", 57]
+        ]},
+        {"key": "Setup", "value": 348, "date": "04\/03\/14", "clients": [
+            ["client3", 66],
+            ["client5", 14]
+        ]},
+        {"key": "Monthly", "value": 198, "date": "04\/03\/14", "clients": [
+            ["client3", 1],
+            ["client7", 55]
+        ]},
+        {"key": "Setup", "value": 99, "date": "04\/04\/14", "clients": [
+            ["client4", 52]
+        ]},
+        {"key": "Monthly", "value": 99, "date": "04\/04\/14", "clients": [
+            ["client2", 58]
+        ]},
+        {"key": "Setup", "value": 0, "date": "04\/05\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/05\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/06\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/06\/14", "clients": null},
+        {"key": "Setup", "value": 398, "date": "04\/07\/14", "clients": [
+            ["client1", 54],
+            ["client7", 59]
+        ]},
+        {"key": "Monthly", "value": 188, "date": "04\/07\/14", "clients": [
+            ["client4", 26],
+            ["client8", 67]
+        ]},
+        {"key": "Setup", "value": 796, "date": "04\/08\/14", "clients": [
+            ["client3", 96],
+            ["client1", 62],
+            ["client9", 78],
+            ["client5", 36]
+        ]},
+        {"key": "Monthly", "value": 386, "date": "04\/08\/14", "clients": [
+            ["client10", 8],
+            ["client3", 62],
+            ["client3", 67],
+            ["client3", 99]
+        ]},
+        {"key": "Setup", "value": 248, "date": "04\/09\/14", "clients": [
+            ["client10", 66],
+            ["client8", 72]
+        ]},
+        {"key": "Monthly", "value": 178, "date": "04\/09\/14", "clients": [
+            ["client10", 56],
+            ["client3", 19]
+        ]},
+        {"key": "Setup", "value": 298, "date": "04\/10\/14", "clients": [
+            ["client6", 72],
+            ["client2", 14]
+        ]},
+        {"key": "Monthly", "value": 198, "date": "04\/10\/14", "clients": [
+            ["client3", 38],
+            ["client3", 47]
+        ]},
+        {"key": "Setup", "value": 696, "date": "04\/11\/14", "clients": [
+            ["client5", 50],
+            ["client9", 13],
+            ["client6", 22],
+            ["client9", 68]
+        ]},
+        {"key": "Monthly", "value": 396, "date": "04\/11\/14", "clients": [
+            ["client7", 76],
+            ["client7", 19],
+            ["client6", 68],
+            ["client1", 63]
+        ]},
+        {"key": "Setup", "value": 0, "date": "04\/12\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/12\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/13\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/13\/14", "clients": null},
+        {"key": "Setup", "value": 398, "date": "04\/14\/14", "clients": [
+            ["client3", 86],
+            ["client6", 11]
+        ]},
+        {"key": "Monthly", "value": 198, "date": "04\/14\/14", "clients": [
+            ["client5", 47],
+            ["client8", 75]
+        ]},
+        {"key": "Setup", "value": 0, "date": "04\/15\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/15\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/16\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/16\/14", "clients": null},
+        {"key": "Setup", "value": 199, "date": "04\/17\/14", "clients": [
+            ["client9", 47]
+        ]},
+        {"key": "Monthly", "value": 99, "date": "04\/17\/14", "clients": [
+            ["client9", 85]
+        ]},
+        {"key": "Setup", "value": 199, "date": "04\/18\/14", "clients": [
+            ["client8", 87]
+        ]},
+        {"key": "Monthly", "value": 99, "date": "04\/18\/14", "clients": [
+            ["client7", 62]
+        ]},
+        {"key": "Setup", "value": 0, "date": "04\/19\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/19\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/20\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/20\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/21\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/21\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/22\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/22\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/23\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/23\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/24\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/24\/14", "clients": null},
+        {"key": "Setup", "value": 597, "date": "04\/25\/14", "clients": [
+            ["client4", 38],
+            ["client4", 42],
+            ["client2", 64]
+        ]},
+        {"key": "Monthly", "value": 297, "date": "04\/25\/14", "clients": [
+            ["client5", 92],
+            ["client6", 34],
+            ["client2", 39]
+        ]},
+        {"key": "Setup", "value": 0, "date": "04\/26\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/26\/14", "clients": null},
+        {"key": "Setup", "value": 0, "date": "04\/27\/14", "clients": null},
+        {"key": "Monthly", "value": 0, "date": "04\/27\/14", "clients": null},
+        {"key": "Setup", "value": 199, "date": "04\/28\/14", "clients": [
+            ["client3", 16]
+        ]},
+        {"key": "Monthly", "value": 99, "date": "04\/28\/14", "clients": [
+            ["client8", 28]
+        ]},
+        {"key": "Setup", "value": 546, "date": "04\/29\/14", "clients": [
+            ["client9", 98],
+            ["client1", 82],
+            ["client2", 71],
+            ["client2", 32]
+        ]},
+        {"key": "Monthly", "value": 366, "date": "04\/29\/14", "clients": [
+            ["client9", 27],
+            ["client10", 60],
+            ["client7", 32],
+            ["client8", 92]
+        ]},
+        {"key": "Setup", "value": 298, "date": "04\/30\/14", "clients": [
+            ["client6", 95],
+            ["client1", 73]
+        ]},
+        {"key": "Monthly", "value": 198, "date": "04\/30\/14", "clients": [
+            ["client10", 63],
+            ["client1", 82]
+        ]}
+    ];
+
+    data.forEach(function (d) {
+        d.date = format.parse(d.date);
+        d.value = +d.value;
+    });
+
+    var layers = stack(nest.entries(data));
+
+    x.domain(d3.extent(data, function (d) {
+        return d.date;
+    }));
+    y.domain([0, d3.max(data, function (d) {
+        return d.y0 + d.y;
+    })]);
+
+    svg.selectAll(".layer")
+        .data(layers)
+        .enter().append("path")
+        .attr("class", "layer")
+        .attr("d", function (d) {
+            return area(d.values);
+        })
+        .style("fill", function (d, i) {
+            return z(i);
+        });
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    var node = svg.selectAll("g")
+        .data(data)
+        .enter()
+        .append("g");
+
+    node.append("circle")
+        .attr("class", "dot")
+        .attr("cx", function (d) {
+            return x(d.date);
+        })
+        .attr("cy", function (d) {
+            return y(d.y0 + d.y);
+        })
+        .attr("r", function (d) {
+            if ((d.y0 + d.y) > 0) {
+                console.log(d);
+                return 2;
+            } else {
+                return 0;
+            }
+        });
+});
